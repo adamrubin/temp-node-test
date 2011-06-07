8553f726f0bcf6b3438ba6c26f6d3bf68f9cf4d1
Adam Rubin <adam@richminds.com>
2011-06-07 18:22:45 -0400

    changed http req

--- web.js
+++ web.js
@@ -1,6 +1,9 @@
 var http = require('http'), faye = require('faye');
 
-var server = http.createServer(function(req, res) {
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('Hello, non-Bayeux request');
+  response.end();
 });
 
 var bayeux = new faye.NodeAdapter({mount: '/bayeux'});
