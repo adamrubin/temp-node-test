29e92087cf2593df11fb37a62233e016511014d8
Adam Rubin <adam@richminds.com>
2011-06-07 18:40:42 -0400

    changed mount point

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var server = http.createServer(function(request, response) {
   response.end();
 });
 
-var bayeux = new faye.NodeAdapter({mount: '/bayeux'});
+var bayeux = new faye.NodeAdapter({mount: '/faye'});
 bayeux.attach(server);
 var port = process.env.PORT || 3000;
 server.listen(port);
