77c3d90d310064031b8ea501edb604348ed320ca
Adam Rubin <adam@richminds.com>
2011-06-07 18:18:29 -0400

    new test

--- web.js
+++ web.js
@@ -1,19 +1,9 @@
 var http = require('http'), faye = require('faye');
 
-var bayeux = new faye.NodeAdapter({
-  mount: '/faye',
-  timeout: 10
+var server = http.createServer(function(req, res) {
 });
 
-// Handle non-Bayeux requests
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Hello, non-Bayeux request');
-  response.end();
-});
-
-var port = process.env.PORT || 3000;
+var bayeux = new faye.NodeAdapter({mount: '/bayeux'});
 bayeux.attach(server);
-server.listen(port, function() {
-	console.log("Listening on " + port);
-});
+// var port = process.env.PORT || 3000;
+server.listen(8000);
